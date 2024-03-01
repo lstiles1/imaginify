f8610cc64159c5886ee422f0ad64c4a13cf5e005
lstiles1 <75706206+lstiles1@users.noreply.github.com>
2024-03-01 09:13:11 -0800

    Update layout.tsx

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -2,19 +2,19 @@ import MobileNav from '@/components/shared/MobileNav'
 import Sidebar from '@/components/shared/Sidebar'
 import { Toaster } from '@/components/ui/toaster'
 
-
-const Layout = ({ children }: {children: React.ReactNode }) => {
+const Layout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <main className='root'>
+    <main className="root">
       <Sidebar />
       <MobileNav />
-        <div className='root-container'>
-            <div className='wrapper'>
-                {children}
-             </div>
+
+      <div className="root-container">
+        <div className="wrapper">
+          {children}
         </div>
-        
-        <Toaster />
+      </div>
+      
+      <Toaster />
     </main>
   )
 }
